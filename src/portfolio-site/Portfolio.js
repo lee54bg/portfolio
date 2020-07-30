9f8ec7891a96d245f5592754fd1a078ca7feb280
B-Rad <lee078bg@yahoo.com>
2020-07-30 11:42:58 -0700

    Added cards to the Portfolio section of the webpage.

--- src/portfolio-site/Portfolio.js
+++ src/portfolio-site/Portfolio.js
@@ -1,12 +1,57 @@
 import React from 'react';
 import NavBar from './NavBar.js';
 
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Box,
+  Grid,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography
+} from '@material-ui/core';
+
+import project1 from '../images/html-css-javascript-lg.jpg';
+
 function Portfolio() {
   return (
-    <div>
+    <Box>
       <NavBar />
-        Hello from Portfolio
-    </div>
+      <Grid container justify="center" alignItems="center">
+        {/* Project 1 */}
+        <Grid item xs={12} sm={8} md={6}>
+          <Card>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                alt="Project 1"
+                height="140"
+                image={project1}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="5">
+                  Project 1
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Project 1
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Share
+                </Button>
+                <Button size="small" color="primary">
+                  Live Demo
+                </Button>
+              </CardActions>
+            </CardActionArea>
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
