807d8550f3f98695b9bacaa69e3ebe37c6b2ea21
B-Rad <lee078bg@yahoo.com>
2020-07-30 17:21:59 -0700

    Added some forms to the contact page.  Will also need to decide if I want to consolidate my resume into the about me section so that only three buttons appear on the side navigation bar.

--- src/portfolio-site/About.js
+++ src/portfolio-site/About.js
@@ -1,12 +1,69 @@
 import React from 'react';
 import NavBar from './NavBar.js';
 
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Box,
+  Grid,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography
+} from '@material-ui/core';
+
+import project1 from '../images/html-css-javascript-lg.jpg';
+import project2 from '../images/javascript-fullstack.jpg';
+
+const projectDetails = [
+  {
+    projectName: "Project 1",
+    projectDetails: "Project 1",
+    projectImage: project1,
+  }, {
+    projectName: "Project 2",
+    projectDetails: "Project 2",
+    projectImage: project2,
+  }, {
+    projectName: "Project 3",
+    projectDetails: "Project 3",
+    projectImage: project1,
+  }
+];
+
+const portfolioStyle = {
+  width: '90%',
+  marginLeft: '10%',
+};
+
+const useStyles = makeStyles({
+  cardContainer: {
+    width: 400,
+  },
+  projectContainer: {
+    paddingLeft: 100,
+  },
+  pro: {
+    color: "Yellow"
+  }
+});
+
 function About() {
+  const classes = useStyles();
+
   return (
-    <div>
+    <Box
+      style={portfolioStyle}
+    >
       <NavBar />
-        Hello from About
-    </div>
+      <Grid container justify="center">
+        <Typography>
+          Hello from project          
+        </Typography>
+      </Grid>
+    </Box>
   );
 }
 
--- src/portfolio-site/Contact.js
+++ src/portfolio-site/Contact.js
@@ -1,12 +1,85 @@
 import React from 'react';
+import SendIcon from '@material-ui/icons/Send';
+
+import { makeStyles } from '@material-ui/core/styles';
+import { 
+  TextField,
+  Typography,
+  Button, 
+  Grid, 
+  Box, 
+  withStyles
+} from '@material-ui/core'
+
 import NavBar from './NavBar.js';
+
+
+
+const portfolioStyle = {
+  width: '90%',
+  marginLeft: '10%',
+};
+
+const InputField = withStyles({
+  root: {
+    "& label.Mui-focused": {
+      color: "tomato",
+    },
+    "& label": {
+      color: "tan",
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "tan",
+      },
+      "&:hover fieldset": {
+        borderColor: "tan",
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "tan",
+      }
+    },
+  },
+})(TextField);
+
 
 function Contact() {
   return (
-    <div>
+    <Box style={portfolioStyle}>
         <NavBar />
-        Hello from Contact
-    </div>
+        <Grid container justify="center">
+        <Box component="form">
+          <Typography variant="h5">
+            Hire or contact me...
+          </Typography>
+          <InputField
+            fullWidth={true}
+            label="Full Name" 
+            variant="outlined"
+            margin="dense"
+            size="medium"
+            />
+          <InputField 
+            fullWidth={true} 
+            label="Email Address" 
+            variant="outlined"
+            margin="dense"
+            size="medium"/>
+          <InputField fullWidth={true} label="Subject" variant="outlined"
+            margin="dense"
+            size="medium"/>
+          <InputField
+            fullWidth={true}
+            label="Message"
+            variant="outlined"
+            margin="dense"
+            size="medium"/>
+          <Button variant="outlined" fullWidth={true} endIcon={<SendIcon />}>
+            Contact Me
+          </Button>
+        </Box>
+        </Grid>
+    </Box>
   );
 }
 
