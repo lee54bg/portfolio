a569d44e88bc44753469ed05bf14a412e05e97df
B-Rad <lee078bg@yahoo.com>
2020-08-14 12:30:39 -0700

    Updates

---
+++ precache-manifest.7999e723f328a5531eb341c8b3d09b27.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d8c8877827be26612a13f39d961649e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b83aa7ae1d816318aefc",
+    "url": "/portfolio/static/css/main.252fcb84.chunk.css"
+  },
+  {
+    "revision": "070ae3e7768f0ee62d9f",
+    "url": "/portfolio/static/js/2.25b44d84.chunk.js"
+  },
+  {
+    "revision": "6c48804b32a0960f7f0c75e8534a0365",
+    "url": "/portfolio/static/js/2.25b44d84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b83aa7ae1d816318aefc",
+    "url": "/portfolio/static/js/main.bdb8cae6.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/portfolio/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/portfolio/static/media/javascript-fullstack.730958a2.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.498aaeb879318b3968fe88fcee9afddb.js"
+  "/portfolio/precache-manifest.7999e723f328a5531eb341c8b3d09b27.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
