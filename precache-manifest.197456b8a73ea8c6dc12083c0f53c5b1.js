cc3c1560ffe29b21e2521ea7e56f0e119978899b
B-Rad <lee078bg@yahoo.com>
2020-08-14 11:37:33 -0700

    Updates

---
+++ precache-manifest.197456b8a73ea8c6dc12083c0f53c5b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3a34c025efccf143b82d75a7c66237",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1617028d3e904886e9f7",
+    "url": "/static/css/main.252fcb84.chunk.css"
+  },
+  {
+    "revision": "3026a5741cb4eb54f623",
+    "url": "/static/js/2.6a0cf399.chunk.js"
+  },
+  {
+    "revision": "6c48804b32a0960f7f0c75e8534a0365",
+    "url": "/static/js/2.6a0cf399.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1617028d3e904886e9f7",
+    "url": "/static/js/main.9eeaa912.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/static/media/javascript-fullstack.730958a2.jpg"
+  }
+]);
